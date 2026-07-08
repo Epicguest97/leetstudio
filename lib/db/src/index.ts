55d78e28077f99b08d55cd9a3ab4208950a98e4b
Epicguest97 <kaushikmehul94@gmail.com>
2026-07-09 01:35:49 +0530

    databse fixed

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 import * as schema from "./schema/index.js";
-import { initializeLocalDatabase } from "./local-bootstrap.js";
 
 const { Pool } = pg;
 const databaseUrl = process.env.DATABASE_URL?.trim();
@@ -11,6 +10,9 @@ export const db = databaseUrl
   ? drizzle(pool!, { schema })
   : (process.env.VERCEL
       ? (() => { throw new Error("DATABASE_URL environment variable is required on Vercel but is not set."); })()
-      : await initializeLocalDatabase());
+      : await (async () => {
+          const { initializeLocalDatabase } = await import("./local-bootstrap.js");
+          return initializeLocalDatabase();
+        })());
 
 export * from "./schema/index.js";
